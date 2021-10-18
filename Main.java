558433e29798ff3a5ba27ca653264e14103399fa
KimJW1 <37143417+Kim-JW@users.noreply.github.com>
2021-10-18 14:30:41 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        for(int i=0; i< s.length()/2; i++){
+          
+        }
+
+
+        return answer;
+    }
+}
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
